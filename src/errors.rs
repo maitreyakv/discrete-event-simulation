3f370d90966caa1cfd09068bee211d5b5c79a58c
Maitreya Venkataswamy <maitreyakv@gmail.com>
2026-06-01 17:31:13 -0400

    refactor: remove from impl

--- src/errors.rs
+++ src/errors.rs
@@ -1,10 +1,5 @@
-use crate::scheduler::CausalityViolation;
-
 #[derive(thiserror::Error, Debug)]
 pub enum DesError<M> {
-    #[error("event was scheduled in the past")]
-    CausalityViolation(#[from] CausalityViolation),
-
     #[error("event processing failed")]
     EventProcessFailure(M),
 }
